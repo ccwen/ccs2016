4521daa4c55bea16987bf52d5d69506204b98363
yapcheahshen <yapcheahshen@gmail.com>
2016-09-11 00:02:47 +0800

    minor change of help message

--- src/righthelp.js
+++ src/righthelp.js
@@ -3,9 +3,9 @@ var E=React.createElement;
 var RightHelp=React.createClass({
 	render:function(){
 		return E("div",{},
-				E("h2",{},"古籍目錄檢索系統"),
-				E("div",{},"資料庫: 劉寧慧 littlehateliu@gmail.com"),
-				E("div",{},E("a",{href:"https://github.com/ksanaforge/ccs2016",target:"_new"},"程式碼"),"：",
+				E("h2",{},"　古籍目錄檢索"),
+				E("div",{},"　資料庫: 劉寧慧 littlehateliu@gmail.com"),
+				E("div",{},"　",E("a",{href:"https://github.com/ksanaforge/ccs2016",target:"_new"},"程式碼"),"：",
 					E("a",{href:"http://www.ksana.tw",target:"_new"},"剎那工坊")
 				)
 			);
